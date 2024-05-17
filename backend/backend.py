287823cb90a2c7ff12d24e51e84c9d60bf0f764c
Siddharth <siddharthpillai2001@gmail.com>
2024-05-17 20:40:14 +0530

    Added add player in frontend and backend

--- backend/backend.py
+++ backend/backend.py
@@ -15,6 +15,29 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 class NumberList(BaseModel):
     numbers: List[Union[float,int]]
 
+class Attributes(BaseModel):
+    overall : float
+    potential : float
+    wage_eur : float
+    age : float
+    height_cm : float
+    weight_kg : float
+    international_reputation : float
+    pace : float
+    shooting : float
+    passing : float
+    dribbling : float
+    physic  : float
+    contribution_type : float
+    league_name : str
+    club_name: str
+    attacking : float
+    skill : float
+    power : float
+    mentality : float
+    goalkeeping  : float
+    movement : float
+
 app = FastAPI()
 
 @app.post("/recommend")
@@ -32,8 +55,18 @@ def _recommend(data:NumberList):
     return teams.to_dict(orient='records')
 
 @app.post("/addPlayer")
-def _recommend(attribute:NumberList):
-    pass
+def _addPlayer(attributes:Attributes):
+    logging.info(f"getting attributes")
+    if not attributes:
+        raise HTTPException(status_code=400, detail="Attributes is empty.")
+    response = requests.get("http://dataset:8081/final_data.csv")
+    df = pd.read_csv(io.StringIO(response.text))
+    logging.info("test")
+    new_row = pd.DataFrame([attributes.dict()])
+    new_row['league_name'] = df[df['club_name']==new_row['club_name'][0]]['league_name'].iloc[0]
+    df = pd.concat([df,new_row],ignore_index=True)
+    logging.info(df.iloc[-1])
+    
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- frontend/frontend.py
+++ frontend/frontend.py
@@ -3,6 +3,39 @@ import requests
 import pandas as pd
 import io
 
+def handle_click(attribute, recommend, contribution):
+    try:
+        response = requests.post("http://backend:8000/addPlayer", json={
+            "overall": attribute[8],
+            "potential": attribute[12],
+            "wage_eur": recommend['wage_eur'],
+            "age": attribute[0],
+            "height_cm": attribute[4],
+            "weight_kg": attribute[16],
+            "international_reputation": attribute[5],
+            "pace": attribute[9],
+            "shooting": attribute[14],
+            "passing": attribute[10],
+            "dribbling": attribute[2],
+            "physic": attribute[11],
+            "contribution_type": contribution,  # Assuming this is fixed or correctly retrieved
+            "league_name": "xyz",
+            "club_name": recommend['team_name'],
+            "attacking": attribute[1],
+            "skill": attribute[15],
+            "power": attribute[13],
+            "mentality": attribute[6],
+            "goalkeeping": attribute[3],
+            "movement": attribute[7]
+        })
+        if response.status_code == 200:
+            st.success(f"Successfully added player to {recommend['team_name']}!")
+        else:
+            st.error("Failed to add player.")
+    except Exception as e:
+        st.error(f"Failed to add player: {e}")
+
+
 def main():
     response = requests.get("http://dataset:8081/final_data.csv")
     if response.status_code == 200:
@@ -30,27 +63,36 @@ def main():
             power = col2.slider("Power", 0.0, 100.0, 50.0)
             mentality = col2.slider("Mentality", 0.0, 100.0, 50.0)
             goal_keeping = col2.slider("Goal Keeping", 0.0, 100.0, 50.0)
+            contribution = col2.slider("Contribution Type",0.0,1.0,step=1.0)
             
         attribute = [age,attack,dribbling,goal_keeping,height,reputation,mentality,movement,overall,pace,passing,physic,potential,power,shooting,skill,weight] 
-        
+
         if st.button("Get Recommendations"):
             try:
                 response = requests.post("http://backend:8000/recommend", json={
                     "numbers": attribute
                 })
+
                 recommendations = response.json()
-                st.write("Top 5 Recommended Players:")
-                for ind, i in enumerate(recommendations):
-                    st.write("Team Name: ", i['team_name'])
-                    st.write("Average Wage in Euros: ", i['wage_eur'])
-                    # st.write("Recommendation Number:", ind + 1)
-                    if st.button("Accept Recommendation", key=f"accept_{ind}", help=str(ind)):
-                        request = requests.post("http://backend:8000/append", json={
-                            "dataToAppend": recommendations[ind]
-                        })
+                st.session_state['recommendations'] = recommendations
+
+                if 'recommendations' in st.session_state:  
+                    st.header("Top 5 Recommended Players:")
+                    for ind, recommend in enumerate(st.session_state['recommendations']):
+                        with st.container():
+                            col1, col2 = st.columns(2)
+                            with col1:
+                                st.markdown(
+                                    f"""
+                                        **Team Name**: {str(recommend['team_name'])}\n
+                                        **Median Salary**(in euros): {recommend['wage_eur']}
+                                    """
+                                )
+                            with col2:
+                                st.button("Accept Recommendation", key=f"btn_{ind}", on_click=handle_click,args=(attribute,recommend,contribution))
             except ValueError as e:
                 st.error("Error: Unable to decode JSON. No recommendations available.")
-            
+        
 
 if __name__ == "__main__":
     main()
